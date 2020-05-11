179b3aa62cf1e874d97bdc121f42e6f1163dba4f
Yi Lin <ly379663442@gmail.com>
2020-05-11 00:00:38 -0400

    Update css

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,6 +5,17 @@ const Home = () => {
   return (
     <Fragment>
       <HomeImage />
+      <div className='home-text text-center all-center '>
+        <h1>Bird Finder</h1>
+        <p>
+          The Bird-Spotting application that lets you discover common, rare, and
+          exotic birds near you.
+        </p>
+        <p>Can you find them ALL?</p>
+        <a className='btn btn-dark p-3 m-1' href='/my search'>
+          Let's Get It Started
+        </a>
+      </div>
     </Fragment>
   );
 };
